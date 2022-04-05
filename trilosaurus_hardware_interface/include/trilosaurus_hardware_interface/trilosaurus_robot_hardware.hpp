0148dbcb1ced90385bca675bfac0b5e4af23b327
poine <poinix@gmail.com>
2022-04-05 14:42:26 +0200

    ros2 hardware interface

---
+++ trilosaurus_hardware_interface/include/trilosaurus_hardware_interface/trilosaurus_robot_hardware.hpp
@@ -0,0 +1,87 @@
+// Copyright (c) 2022, poine
+// Copyright (c) 2022, Stogl Robotics Consulting UG (haftungsbeschränkt) (template)
+// Copyright (c) 2021, ros2_control Development Team (example)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TRILOSAURUS_HARDWARE_INTERFACE__TRILOSAURUS_ROBOT_HARDWARE_HPP_
+#define TRILOSAURUS_HARDWARE_INTERFACE__TRILOSAURUS_ROBOT_HARDWARE_HPP_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp/clock.hpp"
+#include "rclcpp/duration.hpp"
+#include "rclcpp/macros.hpp"
+#include "rclcpp/time.hpp"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+
+#include "trilosaurus_hardware_interface/visibility_control.h"
+
+namespace trilosaurus_hardware_interface
+{
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+class TrilosaurusRobotHardware : public hardware_interface::SystemInterface
+{
+public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(TrilosaurusRobotHardware)
+
+  TRILOSAURUS_HARDWARE_INTERFACE_PUBLIC
+  CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
+
+  TRILOSAURUS_HARDWARE_INTERFACE_PUBLIC
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  TRILOSAURUS_HARDWARE_INTERFACE_PUBLIC
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  TRILOSAURUS_HARDWARE_INTERFACE_PUBLIC
+  CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+
+  TRILOSAURUS_HARDWARE_INTERFACE_PUBLIC
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
+
+  TRILOSAURUS_HARDWARE_INTERFACE_PUBLIC
+  hardware_interface::return_type read() override;
+
+  TRILOSAURUS_HARDWARE_INTERFACE_PUBLIC
+  hardware_interface::return_type write() override;
+
+private:
+  // Parameters for hardware simulation
+  double hw_start_sec_;
+  double hw_stop_sec_;
+  double base_x_, base_y_, base_theta_;
+  
+  std::vector<double> hw_commands_;
+  std::vector<double> hw_positions_;
+  std::vector<double> hw_velocities_;
+
+  // Update loop timing
+  rclcpp::Clock clock_;
+  rclcpp::Time last_timestamp_;
+  rclcpp::Time current_timestamp;
+
+  
+};
+
+}  // namespace trilosaurus_hardware_interface
+
+#endif  // TRILOSAURUS_HARDWARE_INTERFACE__TRILOSAURUS_ROBOT_HARDWARE_HPP_
---
+++ trilosaurus_hardware_interface/src/trilosaurus_robot_hardware.cpp
@@ -0,0 +1,191 @@
+// Copyright (c) 2022, poine
+// Copyright (c) 2022, Stogl Robotics Consulting UG (haftungsbeschränkt) (template)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <limits>
+#include <vector>
+
+#include "trilosaurus_hardware_interface/trilosaurus_robot_hardware.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+namespace trilosaurus_hardware_interface
+{
+CallbackReturn TrilosaurusRobotHardware::on_init(const hardware_interface::HardwareInfo & info)
+{
+  if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS) {
+    return CallbackReturn::ERROR;
+  }
+
+  base_x_ = 0.0;
+  base_y_ = 0.0;
+  base_theta_ = 0.0;
+  
+  hw_start_sec_ = std::stod(info_.hardware_parameters["example_param_hw_start_duration_sec"]);
+  hw_stop_sec_ = std::stod(info_.hardware_parameters["example_param_hw_stop_duration_sec"]);
+  
+  hw_positions_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  hw_velocities_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  hw_commands_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+
+  for (const hardware_interface::ComponentInfo & joint : info_.joints)
+  {
+    // DiffBotSystem has exactly two states and one command interface on each joint
+    if (joint.command_interfaces.size() != 1)
+    {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("DiffBotSystemHardware"),
+        "Joint '%s' has %zu command interfaces found. 1 expected.", joint.name.c_str(),
+        joint.command_interfaces.size());
+      return CallbackReturn::ERROR;
+    }
+
+    if (joint.command_interfaces[0].name != hardware_interface::HW_IF_VELOCITY)
+    {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("DiffBotSystemHardware"),
+        "Joint '%s' have %s command interfaces found. '%s' expected.", joint.name.c_str(),
+        joint.command_interfaces[0].name.c_str(), hardware_interface::HW_IF_VELOCITY);
+      return CallbackReturn::ERROR;
+    }
+
+    if (joint.state_interfaces.size() != 2)
+    {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("DiffBotSystemHardware"),
+        "Joint '%s' has %zu state interface. 2 expected.", joint.name.c_str(),
+        joint.state_interfaces.size());
+      return CallbackReturn::ERROR;
+    }
+
+    if (joint.state_interfaces[0].name != hardware_interface::HW_IF_POSITION)
+    {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("DiffBotSystemHardware"),
+        "Joint '%s' have '%s' as first state interface. '%s' expected.", joint.name.c_str(),
+        joint.state_interfaces[0].name.c_str(), hardware_interface::HW_IF_POSITION);
+      return CallbackReturn::ERROR;
+    }
+
+    if (joint.state_interfaces[1].name != hardware_interface::HW_IF_VELOCITY)
+    {
+      RCLCPP_FATAL(
+        rclcpp::get_logger("DiffBotSystemHardware"),
+        "Joint '%s' have '%s' as second state interface. '%s' expected.", joint.name.c_str(),
+        joint.state_interfaces[1].name.c_str(), hardware_interface::HW_IF_VELOCITY);
+      return CallbackReturn::ERROR;
+    }
+  }
+  clock_ = rclcpp::Clock();
+  return CallbackReturn::SUCCESS;
+}
+
+std::vector<hardware_interface::StateInterface> TrilosaurusRobotHardware::export_state_interfaces()
+{
+  std::vector<hardware_interface::StateInterface> state_interfaces;
+  for (auto i = 0u; i < info_.joints.size(); i++)
+    {
+      state_interfaces.emplace_back(hardware_interface::StateInterface(
+        info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_positions_[i]));
+      state_interfaces.emplace_back(hardware_interface::StateInterface(
+        info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &hw_velocities_[i]));
+    }
+
+  return state_interfaces;
+}
+
+std::vector<hardware_interface::CommandInterface> TrilosaurusRobotHardware::export_command_interfaces()
+{
+  std::vector<hardware_interface::CommandInterface> command_interfaces;
+  for (auto i = 0u; i < info_.joints.size(); i++)
+    {
+      command_interfaces.emplace_back(hardware_interface::CommandInterface(
+        info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &hw_commands_[i]));
+    }
+
+   return command_interfaces;
+}
+
+CallbackReturn TrilosaurusRobotHardware::on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  RCLCPP_INFO(rclcpp::get_logger("TrilosaurusRobotHardware"), "Activating ...please wait...");
+  for (auto i = 0; i < hw_start_sec_; i++)
+    {
+      rclcpp::sleep_for(std::chrono::seconds(1));
+      RCLCPP_INFO(
+	rclcpp::get_logger("DiffBotSystemHardware"), "%.1f seconds left...", hw_start_sec_ - i);
+    }
+  // TODO(anyone): prepare the robot to receive commands
+  // set some default values
+  for (auto i = 0u; i < hw_positions_.size(); i++)
+    {
+      if (std::isnan(hw_positions_[i]))
+	{
+	  hw_positions_[i] = 0;
+	  hw_velocities_[i] = 0;
+	  hw_commands_[i] = 0;
+	}
+    }
+
+  last_timestamp_ = clock_.now();
+  RCLCPP_INFO(rclcpp::get_logger("TrilosaurusRobotHardware"), "Successfully activated!");
+  
+  return CallbackReturn::SUCCESS;
+}
+
+CallbackReturn TrilosaurusRobotHardware::on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  RCLCPP_INFO(rclcpp::get_logger("TrilosaurusRobotHardware"), "Deactivating ...please wait...");
+  // TODO(anyone): prepare the robot to stop receiving commands
+  for (auto i = 0; i < hw_stop_sec_; i++)
+    {
+      rclcpp::sleep_for(std::chrono::seconds(1));
+      RCLCPP_INFO(
+        rclcpp::get_logger("DiffBotSystemHardware"), "%.1f seconds left...", hw_stop_sec_ - i);
+    }
+
+  RCLCPP_INFO(rclcpp::get_logger("TrilosaurusRobotHardware"), "Successfully deactivated!");
+  return CallbackReturn::SUCCESS;
+}
+
+hardware_interface::return_type TrilosaurusRobotHardware::read()
+{
+  current_timestamp = clock_.now();
+  rclcpp::Duration dt = current_timestamp - last_timestamp_;  // Control period
+  last_timestamp_ = current_timestamp;
+#if 0
+  // TODO: read robot states
+#else
+  for (uint i = 0; i < hw_commands_.size(); i++)
+  {
+    // Simulate wheels's movement: simply integrates
+    hw_positions_[i] = hw_positions_[1] + dt.seconds() * hw_commands_[i];
+    hw_velocities_[i] = hw_commands_[i];
+  }
+#endif
+  return hardware_interface::return_type::OK;
+}
+
+hardware_interface::return_type TrilosaurusRobotHardware::write()
+{
+  // TODO(anyone): write robot's commands'
+
+  return hardware_interface::return_type::OK;
+}
+
+}  // namespace trilosaurus_hardware_interface
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(trilosaurus_hardware_interface::TrilosaurusRobotHardware, hardware_interface::SystemInterface)
